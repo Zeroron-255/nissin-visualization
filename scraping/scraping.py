494000e41cdf92a7c33d9595722531090201b913
Zeroron-256 <d0.0b.reirei@gmail.com>
2024-01-18 01:53:11 +0900

    first

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -116,19 +116,3 @@ f = open('information.json', 'w', encoding='UTF-8')
 f.write(all_json)
 
 pprint.pprint(nutrient_text_list)
-
-# nutrient_json = json.dumps(nutrient_text_list, indent=5, ensure_ascii=False)
-# f = open('nutrient.json', 'w', encoding='UTF-8')
-# f.write(nutrient_json)
-
-
-# client = pymongo.MongoClient('localhost', 27017)
-# db = client['i2110707']        # my_dbという新しいデータベースを作成 (my_dbが事前に存在しないことが前提)
-# col = db['nissin']             # my_collectionという新しいコレクションを作成
-
-# for i in range(len(all_list)):
-#     for j in range(len(all_list[i])):
-#         for k in range(len(all_list[i][j])):
-#             col.insert_one(all_list[i][j][k])
-
-# client.close()
